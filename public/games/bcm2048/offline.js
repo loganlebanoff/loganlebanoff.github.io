bb981ce0573bf00a6eb7c4806562f61e0d39e29e
loganlebanoff <loganlebanoff@yahoo.com>
2024-04-08 20:03:28 -0400

    Add games

---
+++ public/games/bcm2048/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1712618822,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/btnplay-sheet0.png",
+		"images/gamebackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/nothing-sheet0.png",
+		"images/edge-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/bloomp-sheet0.png",
+		"images/bloomp-sheet1.png",
+		"images/bloomp-sheet2.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite5-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
